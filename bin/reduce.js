86d9a8e562bf95564d7971ee9876431d2b88e2a3
antleo1995 <antleo1995@gmail.com>
2017-03-28 10:46:33 -0400

    Array iteration
    Still not comfortable enough to write this code myself, but I am feeling
    better about it than when we first started this morning.

--- bin/reduce.js
+++ bin/reduce.js
@@ -9,3 +9,20 @@ const workingArray = examples.numbers.whole
 const result = reduce(workingArray, (p, c, i, a) => p > c ? p : c)
 
 console.log('reduce result', result)
+
+/*
+const examples = require('../lib/examples')
+const reduce = require('../lib/reduce.js')
+
+const loadCities = examples.cities.loadCities
+
+loadCities((cities) => {
+  const city = reduce(cities, (p,c) =>
+ {
+  // + at the start of a string attempts to convert to a number
+  if (+p.population > +c.population) return p
+  return c
+ })
+ console.log(city)
+})
+*/
